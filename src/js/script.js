b92d02bd31c123fa169198c46418613582253e8c
Vitaliy <pyzhyck@gmail.com>
2018-03-30 17:30:02 +0300

    Finished Team page

--- src/js/script.js
+++ src/js/script.js
@@ -13,29 +13,41 @@ $(function() {
         }
     });
 
-    $(".screens-wraper").onepage_scroll({
-        sectionContainer: ".screen",     // sectionContainer accepts any kind of selector in case you don't want to use section
-        easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
-                                         // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-        animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
-        pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-        updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-        beforeMove: function(index) {
-            $('.header, .footer').addClass('on-scroll');
+    if(!$('body').hasClass('no-one-page-effect')) {
+        checkBgColorOfSection(0);
 
-        },  // This option accepts a callback function. The function will be called before the page moves.
-        afterMove: function(index) {
-            $('.screen[data-index='+index+'] .aos-init').addClass('aos-animate');
-            checkBgColorOfSection(index);
-            $('.header, .footer').removeClass('on-scroll');
-        },   // This option accepts a callback function. The function will be called after the page moves.
-        loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-        keyboard: true,                  // You can activate the keyboard controls
-        responsiveFallback: 992,        // You can fallback to normal page scroll by defining the width of the browser in which
-                                         // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-                                         // the browser's width is less than 600, the fallback will kick in.
-        direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
-     });
+        $(".screens-wraper").onepage_scroll({
+            sectionContainer: ".screen",     // sectionContainer accepts any kind of selector in case you don't want to use section
+            easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                                             // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+            animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+            pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+            updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+            beforeMove: function(index) {
+                $('.header, .footer').addClass('on-scroll');
+    
+            },  // This option accepts a callback function. The function will be called before the page moves.
+            afterMove: function(index) {
+                $('.screen[data-index='+index+'] .aos-init').addClass('aos-animate');
+                checkBgColorOfSection(index);
+                $('.header, .footer').removeClass('on-scroll');
+            },   // This option accepts a callback function. The function will be called after the page moves.
+            loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+            keyboard: true,                  // You can activate the keyboard controls
+            responsiveFallback: 992,        // You can fallback to normal page scroll by defining the width of the browser in which
+                                             // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+                                             // the browser's width is less than 600, the fallback will kick in.
+            direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+        });
+
+        if($('section[data-index="1"]').hasClass('screen--dark')) {
+            $('.header').addClass('header--dark');
+            $('.footer').addClass('footer--dark');
+        } else {
+            $('.header').removeClass('header--dark');
+            $('.footer').removeClass('footer--dark');
+        }
+    }
 
 
 
@@ -49,16 +61,6 @@ $(function() {
             $('.img-dark').hide();
             $('.header, .footer').removeClass('on-dark-screen');
         }
-    }
-
-    checkBgColorOfSection(0);
-
-    if($('section[data-index="1"]').hasClass('screen--dark')) {
-        $('.header').addClass('header--dark');
-        $('.footer').addClass('footer--dark');
-    } else {
-        $('.header').removeClass('header--dark');
-        $('.footer').removeClass('footer--dark');
     }
 
     $('[data-js="burger-btn"]').on('click', function(e) {
